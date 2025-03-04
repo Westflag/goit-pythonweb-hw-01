7bc476309d4c9875b7352e55827684bc50109aa0
Andrii Brelyk <andrii.brelyk@lobyco.com>
2025-03-04 13:44:26 +0200

    Homework 01 Factory & SOLID

---
+++ 02-solid.py
@@ -0,0 +1,95 @@
+import logging
+from abc import ABC, abstractmethod
+
+# Налаштування логування
+logging.basicConfig(level=logging.INFO, format="%(message)s")
+logger = logging.getLogger(__name__)
+
+
+class Book:
+    def __init__(self, title: str, author: str, year: str):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __str__(self):
+        return f'Title: {self.title}, Author: {self.author}, Year: {self.year}'
+
+
+class LibraryInterface(ABC):
+    @abstractmethod
+    def add_book(self, book: Book):
+        pass
+
+    @abstractmethod
+    def remove_book(self, title: str):
+        pass
+
+    @abstractmethod
+    def show_books(self):
+        pass
+
+
+class Library(LibraryInterface):
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book: Book):
+        self.books.append(book)
+        logger.info(f'Book added: {book}')
+
+    def remove_book(self, title: str):
+        for book in self.books:
+            if book.title == title:
+                self.books.remove(book)
+                logger.info(f'Book removed: {book}')
+                break
+
+    def show_books(self):
+        if not self.books:
+            logger.info("No books in the library.")
+        for book in self.books:
+            logger.info(str(book))
+
+
+class LibraryManager:
+    def __init__(self, library: LibraryInterface):
+        self.library = library
+
+    def add_book(self, title: str, author: str, year: str):
+        book = Book(title, author, year)
+        self.library.add_book(book)
+
+    def remove_book(self, title: str):
+        self.library.remove_book(title)
+
+    def show_books(self):
+        self.library.show_books()
+
+
+def main():
+    library = Library()
+    manager = LibraryManager(library)
+
+    while True:
+        command = input("Enter command (add, remove, show, exit): ").strip().lower()
+
+        match command:
+            case "add":
+                title = input("Enter book title: ").strip()
+                author = input("Enter book author: ").strip()
+                year = input("Enter book year: ").strip()
+                manager.add_book(title, author, year)
+            case "remove":
+                title = input("Enter book title to remove: ").strip()
+                manager.remove_book(title)
+            case "show":
+                manager.show_books()
+            case "exit":
+                break
+            case _:
+                logger.info("Invalid command. Please try again.")
+
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # goit-pythonweb-hw-01
+## Results
+### 01 Factory
+![img.png](img.png)
+
+### 02 SOLID
+
+![img_1.png](img_1.png)
